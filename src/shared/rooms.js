12c5105f443dee32143cb3dcd0f11729bf7ff7e9
Ivon Badra <ibadra.c@gmail.com>
2021-05-23 22:43:41 -0400

    Rooms array

---
+++ src/shared/rooms.js
@@ -0,0 +1,94 @@
+export const ROOMS = [
+  {
+    id: 0,
+    name: "Athens Suite",
+    description:
+      "Our Athens Suite is designed to give maximum space, comfort and luxury to a large party of guests. Equipped with TV, coffee maker and fridge.",
+    image: "assets/img/room-2.png",
+    icon1: <i className="fa fa-user fa-3x" />,
+    icon2: <i className="fa fa-bed fa-3x" />,
+    icon3: (
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="utensils"
+        class="svg-inline--fa fa-utensils"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 416 512"
+      >
+        <path
+          fill="currentColor"
+          d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 1,
+    name: "Corfu Room",
+    description:
+      "Perfect for small families, the Corfu Room invites you to relax while you plan your itinerary. Equipped with TV, coffee maker and fridge.",
+    image: "assets/img/room-3.png",
+    icon1: <i className="fa fa-user fa-3x" />,
+    icon2: <i className="fa fa-bed fa-3x" />,
+    icon3: (
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="utensils"
+        class="svg-inline--fa fa-utensils"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 416 512"
+      >
+        <path
+          fill="currentColor"
+          d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    name: "Kalamata Room",
+    description:
+      "Perfect for a getaway or a honeymoon, our Kalamata Room is your best option. Equipped with TV, coffee maker and fridge.",
+    image: "assets/img/room-1.png",
+    icon1: <i className="fa fa-user fa-3x" />,
+    icon2: <i className="fa fa-bed fa-3x" />,
+    icon3: (
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="utensils"
+        class="svg-inline--fa fa-utensils"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 416 512"
+      >
+        <path
+          fill="currentColor"
+          d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 3,
+    name: "Ballrooms",
+    description:
+      "The ballrooms are beautifully appointed with crystal chandeliers and a creamy color palette that is sure to compliment your chosen color scheme or preferred décor. The ballrooms can comfortably accommodate up to 500, 250 or 100 guests for a sit-down meal with ample room for a dance floor and staging.",
+    image: "assets/img/ballroom-2.png",
+  },
+  {
+    id: 4,
+    name: "Conference Rooms",
+    description:
+      "TWe can help you make arrangements, from mixed seating for one session, then a communal meeting set-up with lots of screens. And later, something more formal. In between it all, we will design some casual break-out venues plus open networking spaces.",
+    image: "assets/img/conference.png",
+  },
+];

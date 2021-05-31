cb4016bb6e515acad82d7b8a8576a3884505d200
Ivon Badra <ibadra.c@gmail.com>
2021-05-30 22:29:11 -0400

    Hero Component part 1

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -1,22 +1,60 @@
-import React from 'react';
-import { Carousel } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem
+} from 'reactstrap';
 
-function Hero ({heroContent}) {
-    return(
-        <div className="heroBlock">
-            Hero Component Goes Here
-            {/* <Carousel>
-                {heroContent.map((item) => {
-                    return (
-                        <div key={item.id} style={{ backgroundImage: `URL(${item.media})` }} className="container-fluid">
-                            Hello
-                        </div>
-                    );
-                })}
-            </Carousel> */}
-        </div>
+
+const Hero = ({ heroContent }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === heroContent.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? heroContent.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = heroContent.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.id}
+      >
+        <img src={item.image} alt={item.title} className="img-fluid"/>
+      </CarouselItem>
     );
+  });
 
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      pause={false}
+    >
+      
+      {slides}
+      
+    </Carousel>
+  );
 }
 
 export default Hero;
+
+
+
+
+
